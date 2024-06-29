8644aaae320feb95d6dad706afe77b3d999680f3
clecap <clemens.cap@uni-rostock.de>
2024-06-29 17:11:18 +0200

    MP:

--- DanteSettings-used.php
+++ DanteSettings-used.php
@@ -1,29 +1,36 @@
 <?php
- 
+
 #
-# System internal parameters to be used in development
+# System internal parameters to be used in production
 #
 
-$wgDanteOperatingMode      = "Development";
-$wgShowExceptionDetails    = true;
-$wgShowDBErrorBacktrace    = true;
-$wgShowSQLErrors           = true;
-$wgDebugToolbar            = true;
-#$wgShowDebug               = true;
-$wgDevelopmentWarnings     = true;
 
-$wgMessageCacheType        = CACHE_NONE;
+## DEBUG messages
+$wgDanteOperatingMode      = "Production";
+$wgShowExceptionDetails  = false;
+$wgShowDBErrorBacktrace  = false;
+$wgShowSQLErrors         = false;
+$wgDebugToolbar          = false;
+$wgShowDebug             = false;
+$wgDevelopmentWarnings   = false;
 
-$wgParserCacheType         = CACHE_NONE;
-$wgCachePages              = false;
-## $wgMainCache
+## CACHING
+$wgParserCacheType       =  CACHE_ACCEL; 
+$wgCachePages            = true;
+$wgMainCacheType         = CACHE_ACCEL;
 
-$wgAllowVerbose            = true;
+$wgEnableSidebarCache    = false;    # we must not cache sidebar since we often change the sidebar, even dynamically
 
-error_reporting( -1 );
-ini_set( 'display_errors', 1 );
+$wgAllowVerbose            = false;
 
-opcache_reset();
+# we need to invalidate cache when a configuration is changed or when Parsifal cache is cleared.
+# setting this variable ensures this since Parsifal cache clearing special page will touch LocalSettings.php
+$wgInvalidateCacheOnLocalSettingsChange=true;
 
 # how long are we caching responses from the ressource loader. values in seconds
-$wgResourceLoaderMaxage = ['versioned' => 0, 'unversioned' => 0 ];
+$wgResourceLoaderMaxage = [
+  'versioned'   => 30 * 24 * 60 * 60, // 30 days
+  'unversioned' => 5 * 60 ];          // 5 minutes
+
+
+$wgCacheDirectory = "$IP/cache";
