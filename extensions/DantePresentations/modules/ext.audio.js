88a07ed0e41f5c3ce65c79ab5ddc5fe02733ccb4
clecap <clemens.cap@uni-rostock.de>
2024-08-09 14:34:36 +0200

    MP:

---
+++ extensions/DantePresentations/modules/ext.audio.js
@@ -0,0 +1,28 @@
+
+
+
+  
+
+let player;
+
+function init () {
+  console.log ("ext.audio: initialized");
+  player = document.createElement ("audio");
+  player.style= "position:fixed; top:0px; left:0px; width:200px; height:200px; border:1px solid red;";
+
+  document.body.appendChild (player);
+
+}
+
+
+
+
+init ();
+
+
+  console.log ("ext.audio: loaded");
+
+
+
+
+
