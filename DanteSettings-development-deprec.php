2f760ecb9939649ef1d82cd9e945d50a9149847e
clecap <clemens.cap@uni-rostock.de>
2025-03-07 16:25:31 +0100

    MP:

---
+++ DanteSettings-development-deprec.php
@@ -0,0 +1,34 @@
+<?php
+ 
+#
+# System internal parameters to be used in development
+# This is copied to DanteSettings-used.php in Special:ParsifalDebug
+
+$wgDanteOperatingMode      = "Development";
+$wgShowExceptionDetails    = true;
+$wgShowDBErrorBacktrace    = true;
+$wgShowSQLErrors           = true;
+$wgDebugToolbar            = true;
+#$wgShowDebug               = true;
+$wgDevelopmentWarnings     = true;
+
+$wgMessageCacheType        = CACHE_NONE;
+
+$wgParserCacheType         = CACHE_NONE;
+$wgCachePages              = false;
+## $wgMainCache
+
+$wgAllowVerbose            = true;
+
+#error_reporting( -1 );
+error_reporting (E_ALL );  # report all errors AND deprecations
+
+ini_set( 'display_errors', 1 );
+
+
+$wgRawHtml=true;
+
+opcache_reset();
+
+# how long are we caching responses from the ressource loader. values in seconds
+$wgResourceLoaderMaxage = ['versioned' => 0, 'unversioned' => 0 ];
