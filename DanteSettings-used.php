6d9cbcec88b85d980ea0a7317700041c63afd977
Clemens H. Cap <>
2025-12-02 11:36:43 +0000

    s

--- DanteSettings-used.php
+++ DanteSettings-used.php
@@ -1,34 +1,40 @@
 <?php
- 
+
 #
-# System internal parameters to be used in development
+# System internal parameters to be used in production
 # This is copied to DanteSettings-used.php in Special:ParsifalDebug
 
-$wgDanteOperatingMode      = "Development";
-$wgShowExceptionDetails    = true;
-$wgShowDBErrorBacktrace    = true;
-$wgShowSQLErrors           = true;
-$wgDebugToolbar            = true;
-#$wgShowDebug               = true;
-$wgDevelopmentWarnings     = true;
 
-$wgMessageCacheType        = CACHE_NONE;
+## DEBUG messages
+$wgDanteOperatingMode      = "Production";
+$wgShowExceptionDetails  = false;
+$wgShowDBErrorBacktrace  = false;
+$wgShowSQLErrors         = false;
+$wgDebugToolbar          = false;
+$wgShowDebug             = false;
+$wgDevelopmentWarnings   = false;
 
-$wgParserCacheType         = CACHE_NONE;
-$wgCachePages              = false;
-## $wgMainCache
+## CACHING
+$wgParserCacheType       =  CACHE_ACCEL; 
+$wgCachePages            = true;
+$wgMainCacheType         = CACHE_ACCEL;
 
-$wgAllowVerbose            = true;
+$wgEnableSidebarCache    = false;    # we must not cache sidebar since we often change the sidebar, even dynamically
 
-#error_reporting( -1 );
-error_reporting (E_ALL & ~E_DEPRECATED );  # report all errors but not deprecations
+$wgAllowVerbose            = false;
 
-ini_set( 'display_errors', 1 );
-
-
-$wgRawHtml=true;
-
-opcache_reset();
+# we need to invalidate cache when a configuration is changed or when Parsifal cache is cleared.
+# setting this variable ensures this since Parsifal cache clearing special page will touch LocalSettings.php
+$wgInvalidateCacheOnLocalSettingsChange=true;
 
 # how long are we caching responses from the ressource loader. values in seconds
-$wgResourceLoaderMaxage = ['versioned' => 0, 'unversioned' => 0 ];
+$wgResourceLoaderMaxage = [
+  'versioned'   => 30 * 24 * 60 * 60, // 30 days
+  'unversioned' => 5 * 60 ];          // 5 minutes
+
+
+$wgCacheDirectory = "$IP/cache";
+
+# report all errors except deprecated things 
+error_reporting( E_ALL &! E_DEPRECATED );  
+ini_set( 'display_errors', 1 );
