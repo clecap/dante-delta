970898944579e69082403f906da8d5a4e13db580
Clemens H. Cap <>
2025-12-02 15:42:06 +0000

    DanteTree fix

--- extensions/DanteBread/BreadCrumbsHooks.php
+++ extensions/DanteBread/BreadCrumbsHooks.php
@@ -2,11 +2,6 @@
 
 class BreadCrumbsHooks {
 
-
-public static function onOutputPageBeforeHTML( OutputPage &$out, &$text ) {
-  $out->addModuleStyles ( ["ext.DanteBread.styles"] );  // must be marked position:top in extension.json
-}
-
 // NOTE: this injects directly into the header and (2) leads to an immediate synchronous loading
 //       this is necessary since we inject window.doreadNow() a bit lower in order to force synchronouse, non-flickering display
 public static function onOutputPageAfterGetHeadLinksArray ( $tags, OutputPage $out ) { 
@@ -17,6 +12,7 @@ public static function onOutputPageAfterGetHeadLinksArray ( $tags, OutputPage $o
 
 // inject the element where we will show the breadcrum and inject a script tag for calling for its initialization
 public static function onSiteNoticeAfter ( &$siteNotice, $skin) {
+
   $siteNotice .= '<div id="breadcrumbinsert"></div><script>window.doBreadNow()</script>';  
   return false;
 }
@@ -25,6 +21,7 @@ public static function onSiteNoticeAfter ( &$siteNotice, $skin) {
   // add the current page name into the crumbs for the next occasion displaying themn
 public static function onBeforePageDisplay( $output, $skin ) {
   global $wgScriptPath;                     // provide the script with path information which, at this part of the process, is not yet available on the javascript side
+  $output->addModuleStyles ( ["ext.DanteBread.styles"] ); 
   $title = $output->getTitle();
   $pageName = $title->getPrefixedText();    // title incl prefix without underscores
   $pageKey  = $title->getPrefixedDBkey();   // title incl prefix with underscores
