7007b903a584e02a2c5ac61c9af77c87c5597804
Clemens H. Cap <>
2025-12-10 15:52:48 +0000

    q

--- extensions/DantePresentations/endpoints/serviceEndpoint.php
+++ extensions/DantePresentations/endpoints/serviceEndpoint.php
@@ -1,7 +1,11 @@
 <?php
 
 // serviceEndpoint implements a more general endpoint for executing long running scripts with error control possibilities for the user
-// ServiceendpointHelper attaches script to the session and displays an HTML frame in which events can be displayed
+// ServiceendpointHelper attaches a script to the session and displays an HTML frame in which events can be displayed
+
+
+
+
 
 
 // need to use MediaWiki session manager 
@@ -40,7 +44,7 @@ set_exception_handler(function (Throwable $e) {
 
   // now send in event format
   echo "event: php-exception\n";
-  echo 'data: ' . json_encode($payload) . "\n\n";
+  echo 'data: ' . json_encode($payload) . "\n\n";  // double \n required for proper separation of events from each other
   @ob_flush();
   flush();
 });
@@ -48,8 +52,8 @@ set_exception_handler(function (Throwable $e) {
 // echo "event: exception\n"; echo "data: exception test is Stream started\n\n"; @ob_flush(); flush(); // development debug
 
 // Read/write MediaWiki session data
-$cmdString = $session->get( 'Dante_Cmd' );  // a string which encodes in JSON format the array of commands to be executed
-$envString = $session->get ( 'Dante_Env');  // a string which encodes in JSON format the environment in which these commands shall be executed
+$cmdString = $session->get ( 'Dante_Cmd' );  // a string which encodes in JSON format the array of commands to be executed
+$envString = $session->get ( 'Dante_Env' );  // a string which encodes in JSON format the environment in which these commands shall be executed
 
 // convert json text into php array
 $cmdArray = json_decode ( $cmdString, true );
